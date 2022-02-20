3615290e2cc7b623ceb35b4458c5d2d7029a36af
MargaritaSyd <margarita.syddall@gmail.com>
2022-02-20 09:57:56 -0300

    last 10 operations ok

--- public/javascript/allOpTable.js
+++ public/javascript/allOpTable.js
@@ -1,13 +1,12 @@
 
 window.addEventListener('load' , function(){
 
-    let lastRegister = document.getElementById('lastRegister')
     let tableOperations = document.getElementById('tableOperations')
     let outcomeButton = document.getElementById("outcomeButton")
     let incomeButton = document.getElementById('incomeButton')
 
 //function to add operations table
-
+/*
     function addOperationTable(list){
         
         for(let i=0; i<list.length; i++){
@@ -31,10 +30,11 @@ window.addEventListener('load' , function(){
         }
 
     }
+    */
 //
     //last 10 operations:
 
-    lastRegister.addEventListener('click' , function(){
+ //   lastRegister.addEventListener('click' , function(){
     var row0 = tableOperations.insertRow(0); //Initail Cell
    
     var cell1 = row0.insertCell(0); //Date
@@ -56,10 +56,29 @@ window.addEventListener('load' , function(){
     if(parseOperationsList == null){
         alert("no tienes operaciones");
     } else {
-        addOperationTable(parseOperationsList)
+        for(let i=0; i<parseOperationsList.length && i<10; i++){
+            
+            let rowPosition = i+1
+            var rowI = tableOperations.insertRow(rowPosition); // Operation Cell
+
+            var dateI = rowI.insertCell(0)
+            var conceptI = rowI.insertCell(1)
+            var amountI = rowI.insertCell(2)
+            var typeI = rowI.insertCell(3)
+            var editI = rowI.insertCell(4)
+            var deleteI = rowI.insertCell(5)
+        
+            dateI.innerHTML = parseOperationsList[i].newdate
+            conceptI.innerHTML = parseOperationsList[i].newConcept
+            amountI.innerHTML = parseOperationsList[i].newAmount
+            typeI.innerHTML = parseOperationsList[i].newtype
+            editI.innerHTML = 'edit'
+            deleteI.innerHTML = 'delete'
+        }
 
     }
-})
+
+
 
 // Arrays ingresos - egresos          
         let incomeOperations = []
@@ -73,27 +92,6 @@ window.addEventListener('load' , function(){
        }
    // }
 
-    incomeButton.addEventListener('click' , function(){
-        let incomeOperations = []
-
-        var row0 = tableOperations.insertRow(0); //Initail Cell
-       
-        var cell1 = row0.insertCell(0); //Date
-        var cell2 = row0.insertCell(1); //COncept
-        var cell3 = row0.insertCell(2); //Amount
-        var cell4 = row0.insertCell(3); //Type income/outcome
-        var cell5 = row0.insertCell(4); //Edit
-        var cell6 = row0.insertCell(5);  //Delete
-    
-        cell1.innerHTML = "FECHA";   //Cell Date
-        cell2.innerHTML = 'CONCEPTO'; //Cell Concept
-        cell3.innerHTML = 'MONTO'; //Cell Amount
-        cell4.innerHTML = 'TIPO';  //Cell Type
-        cell5.innerHTML = "";  //Cell edit
-        cell6.innerHTML = ''; //cellDelete
-    
    
-      
-
-    })
+    
 })
